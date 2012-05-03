b5e1d154833d53baff1cd7336bc2a92346fe792c
Nick Bauman <nick.bauman@cirrusmachina.com>
2012-05-02 20:21:16 -0500

    project.clj done

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject nongrata "0.1.0-SNAPSHOT"
+  :description "Mozilla Persona (BrowserID) example using Noir"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [javax.mail/mail "1.4.2"]
+                 [noir "1.3.0-beta3"]]
+  :dev-dependencies [[lein-eclipse "1.0.0"]]
+  :jvm-opts ["-server -Xmx512m"] 
+  :main nongrata.server)
+

d2ecbb3628f0c9ece2df607b153cd8a89155b221
Paul Ewing <paul.ewing.ctr@hpcmo.hpc.mil>
2012-05-02 21:05:40 -0500

    Add BrowserID javascript library.

--- src/nongrata/views/common.clj
+++ src/nongrata/views/common.clj
@@ -1,11 +1,12 @@
 (ns nongrata.views.common
   (:use [noir.core :only [defpartial]]
-        [hiccup.page :only [include-css html5]]))
+        [hiccup.page :only [include-css include-js html5]]))
 
 (defpartial layout [& content]
             (html5
               [:head
                [:title "nongrata"]
+               (include-js "https://browserid.org/include.js")
                (include-css "/css/reset.css")]
               [:body
                [:div#wrapper

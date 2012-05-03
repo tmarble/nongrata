09bebbfc8a111c8174a3ad89340dcc22e42baf25
Nick Bauman <nick.bauman@cirrusmachina.com>
2012-05-02 20:27:39 -0500

    skeleton for nongrata site

---
+++ src/nongrata/views/common.clj
@@ -0,0 +1,12 @@
+(ns nongrata.views.common
+  (:use [noir.core :only [defpartial]]
+        [hiccup.page-helpers :only [include-css html5]]))
+
+(defpartial layout [& content]
+            (html5
+              [:head
+               [:title "nongrata"]
+               (include-css "/css/reset.css")]
+              [:body
+               [:div#wrapper
+                content]]))
---
+++ src/nongrata/views/welcome.clj
@@ -0,0 +1,9 @@
+(ns nongrata.views.welcome
+  (:require [nongrata.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to nongrata"]))

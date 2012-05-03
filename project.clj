b2ddebfab5253f65ee21a0f438e8599de62ded17
Brian Maddy <brian@brianmaddy.com>
2012-05-02 20:41:52 -0500

    added cljs

--- project.clj
+++ project.clj
@@ -4,6 +4,17 @@
                  [javax.mail/mail "1.4.2"]
                  [noir "1.3.0-beta3"]]
   :dev-dependencies [[lein-eclipse "1.0.0"]]
+  :plugins [[lein-cljsbuild "0.1.8"]]
+  :cljsbuild {
+              :builds [{
+                        ; The path to the top-level ClojureScript source directory:
+                        :source-path "src-cljs"
+                        ; The standard ClojureScript compiler options:
+                        ; (See the ClojureScript compiler documentation for details.)
+                        :compiler {
+                                   :output-to "war/javascripts/main.js"  ; default: main.js in current directory
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]}
   :jvm-opts ["-server"] 
   :main nongrata.server)
 

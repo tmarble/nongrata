cdaa2b8d00bbb0aad13ee1282d7020ca98710a12
Paul Ewing <paul.ewing.ctr@hpcmo.hpc.mil>
2012-05-02 21:20:27 -0500

    Add javascript source to redo in cljs.

--- src-cljs/nongrata/main.cljs
+++ src-cljs/nongrata/main.cljs
@@ -1,1 +1,30 @@
 (ns nongrata.main)
+
+
+;; $(function() {
+;;   $('#browserid').click(function() {
+;;     navigator.id.get(gotAssertion);
+;;     return false;
+;;   });
+;; });
+
+
+;; function gotAssertion(assertion) {
+;;   // got an assertion, now send it up to the server for verification
+;;   if (assertion !== null) {
+;;     $.ajax({
+;;       type: 'POST',
+;;       url: '/api/login',
+;;       data: { assertion: assertion },
+;;       success: function(res, status, xhr) {
+;;         if (res === null) {}//loggedOut();
+;;           else loggedIn(res);
+;;         },
+;;       error: function(res, status, xhr) {
+;;         alert("login failure" + res);
+;;       }
+;;     });
+;;   } else {
+;;     //loggedOut();
+;;   }
+;; }

a37b11f658b1c3e66e681ea864ddf86937a683b9
Nick Bauman <nick.bauman@cirrusmachina.com>
2012-05-02 20:33:19 -0500

    fixing bad hiccup ns decl, etc

--- src/nongrata/server.clj
+++ src/nongrata/server.clj
@@ -5,7 +5,7 @@
 
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
-        port (Integer. (get (System/getenv) "PORT" "8080"))]
+        port (Integer. (get (System/getenv) "PORT" "11300"))]
     (server/start port {:mode mode
                         :ns 'nongrata})))
 

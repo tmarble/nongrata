0ec9d5870c4d8836e85ed6e9d2cefe7d62934bdc
Paul Ewing <paul.ewing.ctr@hpcmo.hpc.mil>
2012-05-02 21:34:37 -0500

    Add jayq.

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Mozilla Persona (BrowserID) example using Noir"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [javax.mail/mail "1.4.2"]
+                 [jayq "0.1.0-alpha2"]
                  [noir "1.3.0-beta3"]]
   :dev-dependencies [[lein-eclipse "1.0.0"]]
   :plugins [[lein-cljsbuild "0.1.8"]]

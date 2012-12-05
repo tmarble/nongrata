1fe939e103dff31588c49c25d686d73d1cf43d8d
Tom Marble <tmarble@info9.net>
2012-12-04 22:37:30 -0600

    Another attempt at fixing the audience for Jenkins

--- src/nongrata/views/login.clj
+++ src/nongrata/views/login.clj
@@ -16,7 +16,7 @@
   []
   (let [hostname (.. java.net.InetAddress getLocalHost getHostName)]
     (if (= hostname "roku")
-      "info9.net" ;; special case
+      "roku.info9.net" ;; special case
       hostname)))
 
 (defpage login "/login" []

4657fc4fdb8d104ffe6ac92f0ad4bcc011e2f972
Tom Marble <tmarble@info9.net>
2012-12-04 22:25:03 -0600

    Quick fix for audience name on roku

--- src/nongrata/views/login.clj
+++ src/nongrata/views/login.clj
@@ -1,5 +1,5 @@
 (ns nongrata.views.login
-  (:use 
+  (:use
     noir.fetch.remotes
     [clojure.data.json :only [read-json json-str]]
     [clojure.tools.logging :only [error info]]
@@ -11,41 +11,44 @@
     [noir.content.getting-started]
     [noir.session :as session]))
 
-(defn get-hostname 
+(defn get-hostname
   "Returns the hostname of this machine. May work in some circumstances if it's what the public IP address maps to."
   []
-  (.. java.net.InetAddress getLocalHost getHostName))
+  (let [hostname (.. java.net.InetAddress getLocalHost getHostName)]
+    (if (= hostname "roku")
+      "info.net" ;; special case
+      hostname)))
 
 (defpage login "/login" []
   (common/layout
-    [:p 
+    [:p
      (if (not (session/get "browser-id"))
        [:div [:p "You aren't logged in. Login via Persona."]
-        [:a#browserid-link {"href" "#" "title" "Sign-in with BrowserID" "class" "persona-button"} 
+        [:a#browserid-link {"href" "#" "title" "Sign-in with BrowserID" "class" "persona-button"}
          [:span "Sign in with your email"]]
-        [:a#browserid-logout-link {"href" "#" "title" "Log out" "class" "persona-button noshow"} 
+        [:a#browserid-logout-link {"href" "#" "title" "Log out" "class" "persona-button noshow"}
          [:span "Log out"]]]
        [:div [:p "You are signed in!"]
-        [:a#browserid-link {"href" "#" "title" "Sign-in with BrowserID" "class" "persona-button noshow"} 
+        [:a#browserid-link {"href" "#" "title" "Sign-in with BrowserID" "class" "persona-button noshow"}
          [:span "Sign in with your email"]]
-        [:a#browserid-logout-link {"href" "#" "title" "Log out" "class" "persona-button"} 
+        [:a#browserid-logout-link {"href" "#" "title" "Log out" "class" "persona-button"}
          [:span "Log out"]]])]))
 
 ; Successful response from Browser ID should look something like so:
-;{:status 200 
-; :headers 
-; {"content-type" "application/json; charset=utf-8", 
-;  "strict-transport-security" "max-age=2419200", 
-;  "date" "Tue, 08 May 2012 01:19:35 GMT", 
-;  "connection" "close", 
-;  "content-length" "121"}, 
-; :body 
+;{:status 200
+; :headers
+; {"content-type" "application/json; charset=utf-8",
+;  "strict-transport-security" "max-age=2419200",
+;  "date" "Tue, 08 May 2012 01:19:35 GMT",
+;  "connection" "close",
+;  "content-length" "121"},
+; :body
 ; "{\"status\":\"okay\",\"email\":\"foo@bar.com\",\"audience\":\"localhost\",\"expires\":1336440085619,\"issuer\":\"login.persona.org\"}"}
 
 (defremote apilogin [assertion]
   (info (str "recieved assertion: " assertion "\n\nNow submitting back to login.persona.org"))
   (info (str "posting audience '" (get-hostname) "'.\nBe aware that if you're surfing the test site at a different URL, this will fail."))
-  (if-let [bid-response 
+  (if-let [bid-response
            (client/post "https://verifier.login.persona.org/verify"
                         {:form-params {:assertion assertion :audience "localhost"}})]
     (do
@@ -55,11 +58,11 @@
           (do
             (info (str "HTTP 200 OK from login.persona.org: " body-map))
             (if (= "okay" (:status body-map))
-              (do 
+              (do
                 (info "Status: okay, creating authenticated user session...")
                 (session/put! "browser-id" body-map)))
             body-map)
-          (do 
+          (do
             (error (str "http response:" (:status bid-response)))
             body-map))))
     (do
